b50761a5e619b948c50262d79207235a5ab3a73c
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-19 02:56:38 +0530

    Shell Script to Calculate Factorial of a Number ✅

--- 1.sh
+++ 1.sh
@@ -101,6 +101,8 @@ else
     echo "Grade B or lower"
 fi
 
+---Shell Script to Find the Largest of Three Numbers
+
 #!/bin/bash
 echo -n "Enter three numbers: "
 read A B C
@@ -112,3 +114,24 @@ elif [ $B -ge $A ] && [ $B -ge $C ]; then
 else 
     echo "$C is the largest number."
 fi
+
+---Shell Script to Calculate Factorial of a Number
+
+#!/bin/bash
+
+echo -n "Enter a number: "
+read NUM
+
+FACTORIAL=1
+
+if [ $NUM -lt 0 ]; then
+    echo "Factorial is not defined for negative numbers."
+elif [ $NUM -eq 0 ]; then
+    echo "The factorial of 0 is 1."
+fi
+
+for ((i = 1; i <= NUM; i++)); do
+    FACTORIAL= $((FACTORIAL * i))
+done
+
+echo "The factorial of $NUM is: $FACTORIAL"
