f307e33d2bd52ed2bcdce11c087d360cb2d9a4f3
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-19 02:24:03 +0530

    Add script to determine if a number is even or odd ✅

--- 1.sh
+++ 1.sh
@@ -75,3 +75,15 @@ case $command in
         echo "Usage: $0 {start|stop|status}"
         ;;
 esac
+
+---Whether a given number is even or odd.
+
+#!/bin/bash
+echo -n "Enter an integer: "
+read NUM
+
+if [$((NUM % 2)) -eq 0]; then
+    echo "$NUM is an even number."
+else 
+    echo "$NUM is an odd number."
+fi
