084747c609da19b636ba46990daf711bd1bd32bc
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-19 02:27:52 +0530

    Add script to determine grade based on marks using if-then-else✅

--- 1.sh
+++ 1.sh
@@ -87,3 +87,16 @@ if [$((NUM % 2)) -eq 0]; then
 else 
     echo "$NUM is an odd number."
 fi
+
+---if-then-else
+
+#!/bin/bash
+MARKS=85
+
+if [ $MARKS -ge 90 ]; then
+    echo "Grade A+"
+elif [ $MARKS -ge 80 ]; then
+    echo "Grade A"
+else
+    echo "Grade B or lower"
+fi
