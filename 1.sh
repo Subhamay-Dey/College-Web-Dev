36196ffc3d0be1d92c589b7e463993c7d21b268a
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-18 23:42:46 +0530

    Add user input functionality to greeting script ✅

--- 1.sh
+++ 1.sh
@@ -4,3 +4,9 @@ cal 10 2025 - calender of desired month, year.
 #!/bin/bash
 # This is a simple script
 echo "Hello, World!"
+
+--Read command 
+#!/bin/bash
+echo -n "Enter your name: "
+read user_name
+echo "Hello, $user_name!"
