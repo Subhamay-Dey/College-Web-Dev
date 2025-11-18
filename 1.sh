bafbaa6502bd02111ecae992aa71ddd76d42730e
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-19 03:16:11 +0530

    Added shell script to check if a number is prime or not ✅

--- 1.sh
+++ 1.sh
@@ -135,3 +135,27 @@ for ((i = 1; i <= NUM; i++)); do
 done
 
 echo "The factorial of $NUM is: $FACTORIAL"
+
+#!/bin/bash
+
+echo -n "Enter a number: "
+read num
+
+NUM=0
+
+if [ $num -eq 0 ]; then
+    echo "0 is not a prime number."
+elif [ $num -lt 0 ]; then 
+    echo "Negative numbers are not prime."
+else 
+    for ((i=1; i<=num; i++)); do
+        if [ $((num % i)) -eq 0 ]; then
+            NUM=$((NUM + 1))
+        fi
+    done
+        if [ $NUM -eq 2 ]; then 
+            echo "$num is a prime number."
+        else 
+            echo "$num is not a prime number."
+        fi
+fi
