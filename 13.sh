e8e4ebf77e04d1a070866d89987ccf06d5ea692c
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-19 11:31:40 +0530

    Added shell script functionality to reverse a string using for loop ✅

--- 13.sh
+++ 13.sh
@@ -7,3 +7,14 @@ read INPUT_STRING
 REVERSED_STRING_REV=$(echo "$INPUT_STRING" | rev)
 
 echo "Reversed string (using 'rev'): $REVERSED_STRING_REV"
+
+LEN=${#INPUT_STRING}
+REVERSED_LOOP=""
+
+for (( i=$LEN-1; i>=0; i-- )); do
+    # Extract one character at a time, starting from the end
+    CHAR=${INPUT_STRING:$i:1} 
+    REVERSED_LOOP="${REVERSED_LOOP}${CHAR}"
+done
+
+echo "Reverse (using loop): $REVERSED_LOOP"
